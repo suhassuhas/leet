8a3107aefe4e2892a9d1fe83cb0c8d3e269195a2
suhassuhas <39910073+suhassuhas@users.noreply.github.com>
2022-11-13 10:03:53 +0530

    Time: 71 ms (29.64%), Space: 14.1 MB (48.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        def checkempty(string):
-            #print(string)
-            return string != " " and string != ''
-        arr = s.split(" ")
-        arr = arr[::-1]
-        arr = list(filter(checkempty,arr))
-        print(arr)
-        return " ".join(arr)
+        # def checkempty(string):
+        #     #print(string)
+        #     return string != " " and string != ''
+        # arr = s.split(" ")[::-1]
+        # arr = list(filter(checkempty,arr))
+        # return " ".join(arr)
+        return " ".join(s.split()[::-1])
